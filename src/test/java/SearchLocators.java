76795f64a29fdb2be1280fc64a726facd3b0093a
marym <maria_manzhos@tut.by>
2020-10-04 13:56:48 +0300

    add class SearchLocators

---
+++ src/test/java/SearchLocators.java
@@ -0,0 +1,33 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class SearchLocators {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
+        ChromeOptions options = new ChromeOptions();
+        options.setHeadless(false);
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.get("https://jobs.tut.by/");
+    }
+
+    @Test
+    public void findElementById() {
+    }
+
+    @AfterMethod
+    public void closeBrowser(){
+        driver.quit();
+    }
+}
